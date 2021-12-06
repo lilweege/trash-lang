56e0bcfc1fbbed252eb053a9abe1bb56c0da55d3
Luigi Quattrociocchi <lquatt2@gmail.com>
2021-12-06 04:35:26 -0500

    string view utility functions

---
+++ src/stringView.c
@@ -0,0 +1,82 @@
+#include "stringView.h"
+
+#include <string.h>
+
+StringView svNew(char* data, size_t size) {
+    return (StringView) {
+        .data = data,
+        .size = size,
+    };
+}
+
+StringView svFromCStr(char *cstr) {
+    return svNew(cstr, strlen(cstr));
+}
+
+StringView svLeftTrim(StringView *sv, size_t* outNumLines) {
+    size_t x = 0;
+    while (x < sv->size) {
+        char c = sv->data[x];
+        if (!(c == ' ' || c == '\r' || c == '\n' || c == '\t')) {
+            break;
+        }
+        if (outNumLines != NULL && c == '\n')
+            ++*outNumLines;
+        ++x;
+    }
+    return svLeftChop(sv, x);
+}
+
+StringView svLeftChop(StringView *sv, size_t n) {
+    if (n > sv->size) {
+        n = sv->size;
+    }
+    StringView chopped = svNew(sv->data, n);
+    sv->data += n;
+    sv->size -= n;
+    return chopped;
+}
+
+StringView svSubstring(StringView sv, size_t beginIdx, size_t endIdx) {
+    return svNew(sv.data + beginIdx, endIdx - beginIdx);
+}
+
+int64_t svFirstIndexOfChar(StringView sv, char c) {
+    for (int64_t x = 0; x < (int64_t)sv.size; ++x) {
+        if (sv.data[x] == c) {
+            return x;
+        }
+    }
+    return -1;
+}
+
+int64_t svFirstIndexOf(StringView src, StringView str) {
+    if (str.size > src.size) {
+        return -1;
+    }
+    // // TODO:
+    // // strstr is incorrect, ideally something like strnstr would work
+    // // Unfortunately strnstr is not standard, so I may implement
+    // // my own substr match at some point. IIRC some strstr
+    // // implementations use a modified version of the boyer-moore
+    // // char* res = strstr(src.data, str.data);
+    // if (res == NULL)
+    //     return -1;
+    // return res - src.data;
+    
+    // FIXME: naive implementation
+    for (size_t i = 0; i < src.size - str.size; ++i) {
+        if (memcmp(src.data + i, str.data, str.size) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int svCmp(StringView a, StringView b) {
+    if (a.size != b.size)
+        return a.size > b.size ? 1 : -1;
+    // NOTE: memcmp works exactly the same here but doesn't stop at null
+    // characters. In regular use there is no difference between the two
+    return memcmp(a.data, b.data, a.size);
+}
--- src/stringView.h
+++ src/stringView.h
@@ -2,11 +2,24 @@
 #define _STRINGVIEW_H
 
 #include <stddef.h>
+#include <stdint.h>
 
 typedef struct {
     char* data;
     size_t size;
 } StringView;
 
+StringView svNew(char *data, size_t size);
+StringView svFromCStr(char *cstr);
+StringView svLeftTrim(StringView* sv, size_t* outNumLines);
+StringView svLeftChop(StringView* sv, size_t n);
+StringView svSubstring(StringView sv, size_t beginIdx, size_t endIdx);
+// indexOf will return index or -1 if not found
+int64_t svFirstIndexOfChar(StringView sv, char c);
+int64_t svFirstIndexOf(StringView src, StringView str);
+int svCmp(StringView a, StringView b);
+// ...
+
+// TODO: write some tests for sv operations
 
 #endif // _STRINGVIEW_H
