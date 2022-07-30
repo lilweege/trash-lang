b69e5b95857ad32c70608216bfab599b049c31ca
Luigi Quattrociocchi <luigi@quattrociocchi.net>
2022-07-30 00:35:13 -0400

    EBNF

--- src/parser.hpp
+++ src/parser.hpp
@@ -1,2 +1,39 @@
 #pragma once
 
+// Language Grammar
+// TODO: bitwise operators, pointers
+
+// Program          =  { Procedure }
+// Procedure        =  "proc" Identifier "(" [ VarDefn { "," VarDefn } ] ")" [ "->" Type ] Block
+// Block            =  "{" { Statement } "}"
+
+// Statement        =  BlockStatement | LineStatement
+// BlockStmt        =  IfStmt | ForStmt
+// LineStmt         =  Definition | Assignment | Expression
+
+// IfStmt           =  "if" "(" Expression ")" ( Block | Statement ) [ ElseStmt ]
+// ElseStmt         =  "else" "(" Expression ")" ( Block | Statement )
+// ForSmtm          =  "for" "(" LineStmt ";" LineStmt ";" LineStmt ")" ( Block | Statement )
+
+// Expression       =  LogicalTerm { "||" LogicalTerm }
+// LogicalTerm      =  LogicalFactor { "&&" LogicalFactor }
+// LogicalFactor    =  Comparison { ( "==" | "!=" ) Comparison }
+// Comparison       =  Value { ( ">=" | "<=" | ">" | "<" ) Value }
+// Value            =  Term { ( "+" | "-" ) Term }
+// Term             =  Factor { ( "*" | "/" | "%" ) Factor }
+// Factor           =  IntegerLiteral | FloatLiteral | StringLiteral | CharacterLiteral | ProcedureCall | LValue | "(" Expression ")" | ( "-" Factor ) | ( "!" Factor )
+// ProcedureCall    =  Identifier "(" [ Expression { "," Expression } ] ")"
+
+// VarDefn          =  ( "let" | "mut" ) Type Identifier
+// Type             =  Primitive [ Subscript ]
+// LValue           =  Identifier [ Subscript ]
+// Subscript        =  "[" Expression "]"
+// Primitive        =  "u8" | "i64" | "f64"
+
+// Technically not EBNF...
+// LineComment      =  ? ...
+// IntegerLiteral   =  [0-9]+
+// FloatLiteral     =  IntegerLiteral . IntegerLiteral
+// StringLiteral    =  " { AsciiOrEscapeChar } "
+// CharacterLiteral =  ' AsciiOrEscapeChar '
+
