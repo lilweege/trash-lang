5573fd0d49521bc18bd71f558057af7473cdd933
Luigi Quattrociocchi <luigi@quattrociocchi.net>
2023-01-18 15:37:30 -0500

    Generator stub

--- src/generator.cpp
+++ src/generator.cpp
@@ -1,3 +1,20 @@
 #include "generator.hpp"
 #include "compileerror.hpp"
 
+#include <cassert>
+
+void EmitInstructions(fmt::ostream& out, Target target, const std::vector<Instruction>& instructions) {
+    if (target == Target::LINUX_NASM) {
+        assert(0 && "Target LINUX_NASM is not implemented yet.");
+    }
+    if (target == Target::WINDOWS_NASM) {
+        assert(0 && "Target WINDOWS_NASM is not implemented yet.");
+    }
+    if (target == Target::C) {
+        assert(0 && "Target C is not implemented yet.");
+    }
+
+    out.print("...");
+
+    assert(0);
+}
--- src/generator.hpp
+++ src/generator.hpp
@@ -1,3 +1,13 @@
 #pragma once
 
 #include "parser.hpp"
+#include "bytecode.hpp"
+#include <fmt/os.h>
+
+enum class Target {
+    LINUX_NASM,
+    WINDOWS_NASM,
+    C,
+};
+
+void EmitInstructions(fmt::ostream& out, Target target, const std::vector<Instruction>& instructions);
